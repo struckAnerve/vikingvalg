fa56d5c40d7d5de37cee54014c531bfa1d93cda6
Adrian Arnason <struckthenerve@gmail.com>
2013-03-30 21:49:18 +0100

    Byråkratiet vokser, manager for manager

---
+++ Spillet/Vikingvalg/Vikingvalg/InGameManager.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Vikingvalg
+{
+    /// <summary>
+    /// This is a game component that implements IUpdateable.
+    /// </summary>
+    public class InGameManager : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+        IManageSprites spriteService;
+
+        public InGameManager(Game game)
+            : base(game)
+        {
+            GameComponent collisionManager = new CollisionManager(game);
+            Game.Components.Add(collisionManager);
+            Game.Services.AddService(typeof(IManageCollision), collisionManager);
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Initialize()
+        {
+            spriteService = (IManageSprites)Game.Services.GetService(typeof(IManageSprites));
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Player p1 = new Player(new Rectangle(0, 0, 150, 192));
+            spriteService.AddDrawable((Sprite)p1);
+
+            Enemy e1 = new Enemy(new Vector2(300, 200));
+            spriteService.AddDrawable((Sprite)e1);
+            Enemy e2 = new Enemy(new Vector2(700, 300));
+            spriteService.AddDrawable((Sprite)e2);
+
+            base.LoadContent();
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
