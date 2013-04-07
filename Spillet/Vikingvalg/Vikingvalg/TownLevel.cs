08d79d878add71f19cd7e1a6878b2d6e27a3019c
Adrian Arnason <struckthenerve@gmail.com>
2013-04-07 20:07:56 +0200

    Vi begynner med TownLevel

---
+++ Spillet/Vikingvalg/Vikingvalg/TownLevel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace Vikingvalg
+{
+    class TownLevel : InGameLevel
+    {
+        public TownLevel(Player player1, IManageSprites spriteService, IManageCollision collisionService, InGameManager inGameService)
+            : base(player1, spriteService, collisionService, inGameService)
+        { }
+
+        public override void InitializeLevel()
+        {
+            base.InitializeLevel();
+
+        }
+    }
+}
